7ca83994496890941b3d7b4b385ba70b56e49156
zaLTys <povilas.simanskas@gmail.com>
2022-01-22 14:09:15 +0200

    Add project files.

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorStack.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
