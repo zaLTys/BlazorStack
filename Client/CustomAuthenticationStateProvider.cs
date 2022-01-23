6b803b5d52256207277395286b47a321d300c54c
zaLTys <povilas.simanskas@gmail.com>
2022-01-23 08:50:00 +0200

    Custom authentication state provider and top menu display based on whether the user is authorized or not

---
+++ Client/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorStack.Client
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //emptyExample:
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "zaLTys")
+            }, "testAuthenticationType");
+
+            var user = new ClaimsPrincipal(identity);
+            return Task.FromResult(new AuthenticationState(user));           
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Blazored.Toast;
 using BlazorStack.Client;
 using BlazorStack.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,5 +13,8 @@ builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IPointService, PointService>();
 builder.Services.AddScoped<IUnitService, UnitService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
