7fd60a0883a4ab31fb7ed3a9e09ff2beac22d4a2
Povilas Simanskas <ps@frontit.dk>
2022-03-13 16:49:17 +0200

    Child to parent reset

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -5,10 +5,18 @@ namespace BlazorStack.Client.Pages
     public partial class Counter : ComponentBase
     {
         private int currentCount = 0;
+        private string notificationContent = string.Empty;
 
         private void IncrementCount()
         {
             currentCount++;
+        }
+
+
+        private void ResetCount()
+        {
+            currentCount = 0;
+            notificationContent = "Counter has been reset.";
         }
     }
 }
